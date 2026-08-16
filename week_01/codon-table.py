6d8696193546bcb7effc6926dfffaa26ba42c8f9
anjalig2006-web <anjalig.2006@gmail.com>
2026-08-16 12:59:35 +0530

    Create codon-table.py

---
+++ week_01/codon-table.py
@@ -0,0 +1,108 @@
+codon_table = {
+
+    # Phenylalanine
+    "UUU": "F",
+    "UUC": "F",
+
+    # Leucine
+    "UUA": "L",
+    "UUG": "L",
+    "CUU": "L",
+    "CUC": "L",
+    "CUA": "L",
+    "CUG": "L",
+
+    # Isoleucine
+    "AUU": "I",
+    "AUC": "I",
+    "AUA": "I",
+
+    # Methionine
+    "AUG": "M",
+
+    # Valine
+    "GUU": "V",
+    "GUC": "V",
+    "GUA": "V",
+    "GUG": "V",
+
+    # Serine
+    "UCU": "S",
+    "UCC": "S",
+    "UCA": "S",
+    "UCG": "S",
+    "AGU": "S",
+    "AGC": "S",
+
+    # Proline
+    "CCU": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+
+    # Threonine
+    "ACU": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+
+    # Alanine
+    "GCU": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+
+    # Tyrosine
+    "UAU": "Y",
+    "UAC": "Y",
+
+    # Histidine
+    "CAU": "H",
+    "CAC": "H",
+
+    # Glutamine
+    "CAA": "Q",
+    "CAG": "Q",
+
+    # Asparagine
+    "AAU": "N",
+    "AAC": "N",
+
+    # Lysine
+    "AAA": "K",
+    "AAG": "K",
+
+    # Aspartic acid
+    "GAU": "D",
+    "GAC": "D",
+
+    # Glutamic acid
+    "GAA": "E",
+    "GAG": "E",
+
+    # Cysteine
+    "UGU": "C",
+    "UGC": "C",
+
+    # Tryptophan
+    "UGG": "W",
+
+    # Arginine
+    "CGU": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",
+    "AGA": "R",
+    "AGG": "R",
+
+    # Glycine
+    "GGU": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+
+    # Stop codons
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP"
+}
